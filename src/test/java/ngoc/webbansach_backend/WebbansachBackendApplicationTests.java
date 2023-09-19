cc04e06a6a07e1212a43e60aa37989972111f5d9
ngoc <ngoc2001bui@gmail.com>
2023-09-19 16:21:00 +0700

    Tao du an moi

---
+++ src/test/java/ngoc/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ngoc.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
