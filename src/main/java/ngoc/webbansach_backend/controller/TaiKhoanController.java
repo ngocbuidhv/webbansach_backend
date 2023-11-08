17a205566693169195b8e4cc35ef866792d1deb0
ngocbuidhv <ngoc2001bui@gmail.com>
2023-11-08 10:08:47 +0700

    update CrossOrigin

--- src/main/java/ngoc/webbansach_backend/controller/TaiKhoanController.java
+++ src/main/java/ngoc/webbansach_backend/controller/TaiKhoanController.java
@@ -5,10 +5,7 @@ import ngoc.webbansach_backend.service.TaiKhoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tai-khoan")
@@ -16,6 +13,7 @@ public class TaiKhoanController {
     @Autowired
     private TaiKhoanService taiKhoanService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/dang-ky")
     public ResponseEntity<?> dangKyNguoiDung(@Validated  @RequestBody NguoiDung nguoiDung){
         ResponseEntity<?> response = taiKhoanService.dangKyNguoiDung(nguoiDung);
